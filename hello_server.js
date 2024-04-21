99ee6a9111f8927a60b92017eaa982687d2443f6
“hope-rogan” <rogan.hope@gmail.com>
2024-04-21 11:47:06 -0400

    changed port to 3000

--- hello_server.js
+++ hello_server.js
@@ -1,12 +1,15 @@
 var http = require('http');
 
-var port = 8080;
+// var port = 8080;
+
+var port = process.env.PORT || 3000;
 
 console.log("tesing!")
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello Strange World!');
+    res.write('<h2> helloooo </h2>');
     res.end();
 
 });
