c4a713d77ce9da5958023bf12cef944cfebf2a6d
“hope-rogan” <rogan.hope@gmail.com>
2024-04-21 11:55:32 -0400

    changed to 8080

--- hello_server.js
+++ hello_server.js
@@ -1,15 +1,6 @@
 var http = require('http');
-
-// var port = 8080;
-
-var port = process.env.PORT || 3000;
-
-console.log("tesing!")
-
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello Strange World!');
-    res.write('<h2> helloooo </h2>');
-    res.end();
-
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hello!');
+  res.end();
+}).listen(8080);
