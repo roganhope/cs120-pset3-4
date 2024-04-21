771b58d5c890373862861be47586ca56b2933083
“hope-rogan” <rogan.hope@gmail.com>
2024-04-21 12:02:15 -0400

    back to heroku port

--- hello_server.js
+++ hello_server.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var port = process.env.PORT || 3000;
+
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('Hello!');
   res.end();
-}).listen(8080);
+}).listen(port);
