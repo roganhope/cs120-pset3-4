62b2efd2fe2bb0be34198e10cc41438193d2b023
“hope-rogan” <rogan.hope@gmail.com>
2024-04-22 12:21:04 -0400

    changed .render name for case sensitivity

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const app = express()
 
 
 app.set('views', path.join(__dirname, 'views')) 
-
 app.set('view engine', 'ejs') 
-
 app.use(express.static('public'));
 
 // var favicon = require('serve-favicon')
@@ -17,7 +15,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){ 
-    res.render('Home', { 
+    res.render('home', { 
         title: 'Home Page'
     }) 
 }) 
@@ -25,22 +23,25 @@ app.get('/', function(req, res){
 
 app.post('/process', async function(req, res) {
     const info = req.body.the_name;
-    
     try {
         const processedInfo = await controller.processInfo(info);
         res.render('process', { processedInfo: processedInfo });
     } catch (error) {
-      
         console.error('Error processing information:', error);
         res.status(500).send('Error processing information');
     }
 });
 
 
-port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
-app.listen(port, function(error){ 
-    if(error) throw error 
-    console.log("Server created Successfully"); 
-}) 
+// port = process.env.PORT || 3000;
+// console.log("using port " + port )
+// app.listen(port, function(error){ 
+//     if(error) throw error 
+//     console.log("Server created Successfully"); 
+// }); 
 
