47be9425b0cbec59bb9025bc5e3855d4c8c053b5
“hope-rogan” <rogan.hope@gmail.com>
2024-04-22 12:08:26 -0400

    removing the favicon

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const app = express()
 
 
 app.set('views', path.join(__dirname, 'views')) 
+
 app.set('view engine', 'ejs') 
 
 app.use(express.static('public'));
 
-var favicon = require('serve-favicon')
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+// var favicon = require('serve-favicon')
+// app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
